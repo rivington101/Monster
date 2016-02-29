c01341a13ae0d83096e898c1f2152a562551a368
Peter Rule <peterrule27@gmail.com>
2016-02-29 19:21:52 +0000

    itemDroppedOnCharacter selector issue

---
+++ MyMonster/MonsterImg.swift
@@ -0,0 +1,58 @@
+//
+//  MonsterImg.swift
+//  MyMonster
+//
+//  Created by Peter Rule on 26/02/2016.
+//  Copyright © 2016 Peter Rule. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MonsterImg: UIImageView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder adecoder: NSCoder){
+        super.init(coder: adecoder)
+        playIdleAnimation()
+    }
+    
+    func playIdleAnimation() {
+        
+        self.image = UIImage(named: "idle4.png")
+        
+        self.animationImages = nil
+        
+        var imgArray = [UIImage]()
+        for var x = 1; x <= 4; x++ {
+            let img = UIImage(named: "idle\(x).png")
+            imgArray.append(img!)
+        }
+        
+        self.animationImages = imgArray
+        self.animationDuration = 0.8
+        self.animationRepeatCount = 0
+        self.startAnimating()
+
+    }
+    
+    func playDeathAnimation() {
+        
+        self.image = UIImage(named: "dead5.png")
+        
+        self.animationImages = nil
+        
+        var imgArray = [UIImage]()
+        for var x = 1; x <= 5; x++ {
+            let img = UIImage(named: "dead\(x).png")
+            imgArray.append(img!)
+        }
+        
+        self.animationImages = imgArray
+        self.animationDuration = 0.8
+        self.animationRepeatCount = 1
+        self.startAnimating()
+    }
+}
--- MyMonster/ViewController.swift
+++ MyMonster/ViewController.swift
@@ -5,17 +5,34 @@
 //  Created by Peter Rule on 25/02/2016.
 //  Copyright © 2016 Peter Rule. All rights reserved.
 //
+// Learnt
+// Size classes
+// moving stacks depending on portrait or landscape
+// making a stack horiz in portrait & vertical in landscape
+// moving objects around screen
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var monsterImg: MonsterImg!
+    @IBOutlet weak var foodImg: DragImg!
+    @IBOutlet weak var heartImg: DragImg!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    }
+        foodImg.droptarget = monsterImg
+        heartImg.droptarget = monsterImg
+        
+        //NSNotificationCenter.defaultCenter().addObserver(self, selector: "itemDroppedOnCharacter", name: "onTargetDropped", object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "itemDroppedOnCharacter", name: "onTargetDropped", object: nil)
+            }
 
-    
+    func itemDroppedOnCharacter(notif: AnyObject) {
+        print("ITEM DROPPED ON CHARACTER")
+    }
 
 
 }
